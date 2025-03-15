de72c6eb1437eeb7c25a62205b60b1385bf825b1
SekharK <sekhar.kandimalla@gmail.com>
2025-03-15 07:34:35 +0530

    added converrsion from single token id to string

--- tokens.py
+++ tokens.py
@@ -12,6 +12,10 @@ text = "Hello"
 token_ids = encoder.encode(text)
 substrings = [encoder.decode([tid]) for tid in token_ids]
 
+#The cl100k_base tokenizer has 100,256 tokens. Retrieve the string corresponding to token ID
+string = encoder.decode([20000])
+
 print("Text", text)
 print("Substrings:", substrings)
 print("Token IDs:", token_ids)
+print("Token string:", string)
